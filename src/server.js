cc845c1e320c75abd111d301a6e50e1b0a20723f
josh.dev <joshtowson16@gmail.com>
2024-11-24 18:23:16 -0300

    .

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const PORT = ENVIROMENT.PORT || 3000
 
 app.use(cors())
 app.use(express.json({limit: '5mb'}))
-
+// app.use(verifyApikeyMiddleware)
 
 
 app.use('/api/status', statusRouter)
@@ -23,7 +23,7 @@ app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
 
 
-
+ProductRepository.getProducts()
 
 app.listen(PORT, () => {
     console.log(`El servidor se esta escuchando en http://localhost:${PORT}`)
