fc006f65ed3aea9baa834a1943d5f50a8db115fd
josh.dev <joshtowson16@gmail.com>
2024-11-24 19:25:56 -0300

    .

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const PORT = ENVIROMENT.PORT || 3000
 
 app.use(cors())
 app.use(express.json({limit: '5mb'}))
-// app.use(verifyApikeyMiddleware)
+ app.use(verifyApikeyMiddleware)
 
 
 app.use('/api/status', statusRouter)
