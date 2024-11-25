b2afcb37b58788d85cf99a6b2d42a9e1f8e86ce9
josh.dev <joshtowson16@gmail.com>
2024-11-25 20:19:06 -0300

    email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,49 +3,40 @@ import User from "../models/user.model.js"
 import ResponseBuilder from "../utils/builders/responseBuilder.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import sendEmail from "../utils/mail.util.js"
+import { sendEmail } from "../utils/mail.util.js"
 import UserRepository from "../repositories/user.repository.js"
 
 
 
 
+
 export const registerUserController = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-
-        if (!email) {
+    try{
+        const {name, email, password} = req.body
+        /* Hacer validacion */
+        if(!email){
             const response = new ResponseBuilder()
-                .setOk(false)
-                .setStatus(400)
-                .setMessage('Bad request')
-                .setPayload({
+            .setOk(false)
+            .setStatus(400)
+            .setMessage('Bad request')
+            .setPayload(
+                {
                     detail: 'El email no es valido'
-                })
-                .build();
-            return res.status(400).json(response);
+                }
+            )
+            .build()
+            return res.status(400).json(response)
         }
 
-        const existingUser = await User.findOne({ email });C
-        if (existingUser) {
-            const response = new ResponseBuilder()
-                .setOk(false)
-                .setStatus(409)
-                .setMessage('Conflict')
-                .setPayload({
-                    detail: 'El email ya está registrado'
-                })
-                .build();
-            return res.status(409).json(response);
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10)
         const verificationToken = jwt.sign(
-            { email },
-            ENVIROMENT.JWT_SECRET,
-            { expiresIn: '1d' }
-        );
-        const url_verification = `http://localhost:${ENVIROMENT.PORT}/api/auth/verify/${verificationToken}`;
-
+            {
+                email: email
+            }, ENVIROMENT.JWT_SECRET, {
+            expiresIn: '1d'
+        })
+        const url_verification = `http://localhost:${ENVIROMENT.PORT}/api/auth/verify/${verificationToken}`
+        if (User.findOne == false) {
         await sendEmail({
             to: email,
             subject: 'Valida tu correo electronico',
@@ -53,40 +44,48 @@ export const registerUserController = async (req, res) => {
             <h1>Verificacion de correo electronico</h1>
             <p>Da click en el boton de abajo para verificar</p>
             <a 
-                style='background-color: black; color: white; padding: 5px; border-radius: 5px;'
+                style='background-color: 'black'; color: 'white'; padding: 5px; border-radius: 5px;'
                 href="${url_verification}"
             >Click aqui</a>
             `
-        });
+        })  
+        }
+        
 
         const newUser = new User({
             name,
             email,
             password: hashedPassword,
-            verificationToken,
+            verificationToken: verificationToken,
             emailVerified: false
-        });
+        })
 
-        await newUser.save();
+        //Metodo save nos permite guardar el objeto en la DB
+        await newUser.save()
 
         const response = new ResponseBuilder()
-            .setOk(true)
-            .setStatus(201)
-            .setMessage('Created')
-            .setPayload({})
-            .build();
-        return res.status(201).json(response);
-    } catch (error) {
-        const response = new ResponseBuilder()
-            .setOk(false)
-            .setStatus(500)
-            .setMessage('Internal server error')
-            .setPayload({
-                detail: error.message
-            })
-            .build();
-        return res.status(500).json(response);
+        .setOk(true)
+        .setStatus(200)
+        .setMessage('Created')
+        .setPayload({})
+        .build()
+        return res.status(201).json(response)
     }
+    catch(error){
+        const response = new ResponseBuilder()
+        .setOk(false)
+        .setStatus(500)
+        .setMessage('Internal server error')
+        .setPayload(
+            {
+                detail: error.message
+                
+            }
+        )
+        .build()
+        return res.status(500).json(response)
+    }
+
 }
 
 
