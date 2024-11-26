221f239842dc121348a5da30ebc3f9dc0fa1907b
josh.dev <joshtowson16@gmail.com>
2024-11-26 01:17:30 -0300

    email

--- src/repositories/product.repository.js
+++ src/repositories/product.repository.js
@@ -8,9 +8,28 @@ class ProductRepository{
     }
 
     static async getProductById(id){
-        return Product.findById(id)
-    }
-
+        try {
+            const { id } = req.params;
+    
+            // Validar si el ID es un ObjectId válido
+            if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+                return res.status(400).json({ detail: 'ID no válido' });
+            }
+    
+            // Buscar el producto en la base de datos
+            const product = await Product.findById(id);
+    
+            if (!product) {
+                return res.status(404).json({ detail: 'Producto no encontrado' });
+            }
+    
+            res.status(200).json({ product });
+        } catch (error) {
+            console.error('Error al obtener el producto:', error);
+            res.status(500).json({ detail: 'Error interno del servidor' });
+        }
+    };
+        
     static async createProduct(product_data){
         const new_product = new Product(product_data)
         return new_product.save()
