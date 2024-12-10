f010f268e0aba54b9b56845db7d6b86989c7bc57
josh.dev <joshtowson16@gmail.com>
2024-12-09 21:05:46 -0300

    links del mail

--- src/server.js
+++ src/server.js
@@ -13,7 +13,12 @@ import ProductRepository from "./repositories/product.repository.js";
 const app = express();
 const PORT = ENVIROMENT.PORT || 3000
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: ENVIROMENT.URL_FRONT
+    }
+))
+
 app.use(express.json({limit: '5mb'}))
  app.use(verifyApikeyMiddleware)
 
